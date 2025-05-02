e7a87f499f15c1ce9f157d6007f21005239c5a04
kozhariks <kozhariks@yandex.by>
2025-05-02 12:51:10 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class Config:
+    TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
+    OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+    OPENAI_MODEL = os.getenv("OPENAI_MODEL", "gpt-4-turbo-preview")
